4eedfabc18a2c63410cbb2ac7324cf10ef583e9a
Sihilel H <sihilelh@gmail.com>
2025-10-30 23:45:03 +0530

    feat: Implemented the orchestrator with sin oscillator.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use std::error::Error;
 
 mod cli;
 mod orchestrator;
+mod oscillator;
 mod wav;
 
 const SAMPLE_RATE: u32 = 44100;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let music_input = get_music_input()?;
-    println!("{:#?}", music_input);
+    let pcm_samples = music_input.pcm_samples(SAMPLE_RATE);
+    wav::write("output/simple_octave.wav", &pcm_samples, SAMPLE_RATE)?;
     Ok(())
 }
--- src/orchestrator.rs
+++ src/orchestrator.rs
@@ -1,3 +1,4 @@
+use crate::oscillator::SinOscillator;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -5,6 +6,7 @@ pub struct Note {
     id: u8,
     octave: u8,
     beats: f64,
+    amplitude: f64,
 }
 
 impl Note {
@@ -21,6 +23,23 @@ pub struct Orchestrator {
     notes: Vec<Note>,
 }
 
-// impl Orchestrator {
-//     pub fn pcm_samples(&self) -> Vec<i16> {}
-// }
+impl Orchestrator {
+    pub fn pcm_samples(&self, sample_rate: u32) -> Vec<i16> {
+        let mut samples: Vec<i16> = Vec::new();
+        let seconds_per_beat = 60.0 / self.bpm as f64;
+
+        for note in &self.notes {
+            let wave = SinOscillator {
+                amplitude: note.amplitude,
+                frequency: note.frequency(),
+                sample_rate: sample_rate,
+            };
+            let duration = note.beats * seconds_per_beat;
+            let samples_per_note = (duration * sample_rate as f64) as u32;
+            for i in 0..samples_per_note {
+                samples.push(wave.pcm_sample(i));
+            }
+        }
+        samples
+    }
+}
